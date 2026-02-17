975f44bcddaedfe975bdd8632ca41e760f20bf47
ghofranahmed <ghofranahmed11ft@gmail.com>
2026-02-17 02:13:07 +0200

    small change

--- database/migrations/2026_02_16_202936_add_role_and_notifications_to_users_table.php
+++ database/migrations/2026_02_16_202936_add_role_and_notifications_to_users_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
 
 return new class extends Migration
 {
@@ -20,7 +22,7 @@ return new class extends Migration
         });
 
         // Migrate existing is_admin to role
-        \DB::table('users')->where('is_admin', true)->update(['role' => 'admin']);
+        DB::table('users')->where('is_admin', true)->update(['role' => 'admin']);
     }
 
     /**
